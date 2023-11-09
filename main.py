9c6d6b4499fbefc2ab9a16d840c6a92ea38b4647
gelileo <95984858+gelileo@users.noreply.github.com>
2023-11-09 18:24:30 +0000

    update

--- main.py
+++ main.py
@@ -1,27 +1,78 @@
-def timeTable(num):
-  for i in range(1, num + 1):
-    for j in range(1, num + 1):
-      if i != j:
-        print(f"{i}x{j}={i*j}", end=', ')
-      else:
-        print(f"{i}x{j}={i*j}")
-        break
+from re import X
 
 
-#for i in range(1,10):
-#  timeTable(i)
-#  print()
+list = [i for i in range(1,10)]
+print(list)
+
+# Output:
+# 1 2 3
+# 4 5 6 
+# 7 8 9
+#
+
+list = ['X' for _ in range(9)]
+print (list)
+#  X | O | X
+# ----------
+#  O | X | O
+# ----------
+#  X | O | X
 
 
-def timeTable2(num):
-  for i in range(1, num + 1):
-    for j in range(1, i):
-      print(f"{i}x{j}={i*j}", end=', ')
-    print(f"{i}x{i}={i*i}")
 
 
-# timeTable2(8)
 
-# arr = [[0 for _ in range(3)] for _ in range(3)]
-# arr[1][1] = 5
+# list1 = [1,2,3,4,5,6,7,8,9,24, 636, 15, 3647]
+# # print(f'list1: {list1}')
+
+# list2 = [ i for i in range(1,10) if i<5]
+# print(f'list2: {list2}')
+
+# list3 = []
+# for i in range(1,10):
+#   if i < 5:
+#     list3.append(i+100)
+
+# print(f'list3: {list3}')
+
+
+# ODD
+# list2 = [i for i in list1 if i%2 != 0]
+# print(f'list2: {list2}')
+
+
+# print('-'*9)
+
+# # arr = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+# arr = ['-' for _ in range(9)] # this gives you the same list
+
+# print(len(arr))
+# print(arr)
+# for i in arr:
+#   print(i, end='')
+# print()
+
+
+
+  
+
+
+
+# arr = ['1','2','3']
+
+# print(arr[0]) # 1
+# print(arr[2]) # 3
+# # print(arr[3]) # err: list index out of range
+
+# arr.append('4')
+# print(arr)
+
+# arr[0] = '0'
+# print(arr)
+
+
+# # arr[4] = '5' # out of range
+# arr.append('5')
+# # print(arr)
+# arr[4] = 'x'
 # print(arr)
