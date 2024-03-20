29c87722511a47e8d72ed9cdeeaee80961828ec3
galileo <fll4klu@gmail.com>
2024-03-19 22:49:02 -0400

    updated tic-tac-toe

--- tmp/tictactoe.py
+++ tmp/tictactoe.py
@@ -10,7 +10,7 @@ cell_padding = 10
 screen_width = 3 * cell_size - 2 * cell_padding
 screen_height = screen_width
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("TIC TAc TOE")
+pygame.display.set_caption("TIC TAC TOE")
 
 # Colors
 WHITE = (255, 255, 255)
@@ -91,12 +91,71 @@ def draw_shape(row, col):
         )
 
 
+#
 # Variable to keep track of current player
 current_player = "X"
 
-# Main loop
+# Switch player
+
+
+def switch_player():
+    global current_player
+    current_player = "O" if current_player == "X" else "X"
+    print(f"Now waiting for player {current_player}:")
+
+# Check win
+
+
+def check_row(board):
+    global winner
+    for row in board:
+        if all(cell == current_player for cell in row):
+            return True
+
+    return False
+
+
+def check_column(board):
+    for col in range(3):
+        if all(board[row][col] == current_player for row in range(3)):
+            return True
+    return False
+
+
+def check_diagnal(board):
+    if all(board[i][i] == current_player for i in range(3)) or all(board[i][2 - i] == current_player for i in range(3)):
+        return True
+    return False
+
+
+def check_tie(board):
+    return all(cell != '' for row in board for cell in row)
+
+
+def check_win():
+    if check_diagnal(board) or check_row(board) or check_column(board):
+        print(f"The winner is {current_player}")
+        return True
+    else:
+        return False
+
+
 clock = pygame.time.Clock()
+
+
+def redraw():
+    draw_board()
+    pygame.display.flip()
+    clock.tick(30)
+
+
+# Init the screen
+screen.fill(WHITE)
+draw_board()
+pygame.display.flip()
 running = True
+game_over = False
+# Main loop
 while running:
     # Handle events
     for event in pygame.event.get():
@@ -106,19 +165,22 @@ while running:
             mouse_pos = pygame.mouse.get_pos()
             row, col = get_cell_position(mouse_pos)
             print("Clicked cell:", row, col)
-            if board[row][col] == "":
+            if not game_over and board[row][col] == "":
                 board[row][col] = current_player
-                current_player = "O" if current_player == "X" else "X"
+                needs_redraw = True
+                if check_win():
+                    game_over = True
+                    redraw()
+                    break
 
-    # Clear the screen
-    screen.fill(WHITE)
+                if check_tie(board):
+                    game_over = True
+                    redraw()
+                    break
 
-    # Draw the game board
-    draw_board()
+                switch_player()
+                redraw()
 
-    # Update the display
-    pygame.display.flip()
-    clock.tick(30)
 
 # Quit Pygame
 pygame.quit()
