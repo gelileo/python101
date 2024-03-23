db10849a4518285b77a931286d40498189b97743
gelileo <95984858+gelileo@users.noreply.github.com>
2024-03-23 16:24:41 +0000

    update

--- main.py
+++ main.py
@@ -1,86 +1,57 @@
-import json
+import pygame
 
-class Contact:
-  def __init__(self, phone, email):
-    self.phone = phone
-    self.email = email
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+clock = pygame.time.Clock()
+running = True
+
+x = 40 
+y = 40
+radius = 20
+step = radius
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill('blue')
+    # pygame.draw.circle(screen, "red", (40,40), 20)
+
+    pygame.draw.circle(screen, "red", (x,y), radius)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        y -= step
+    if keys[pygame.K_DOWN]:
+        y += step
+    if keys[pygame.K_LEFT]:
+        x -= step
+    if keys[pygame.K_RIGHT]:
+        x += step
   
-class Address:
-  def __init__(self, street, city, state, zip):
-    self.street = street
-    self.city = city
-    self.state = state
-    self.zip = zip 
+  # rect(surface, color, rect) -> Rect
+    # pygame.draw.rect(screen, (200, 255, 127, 128), (200, 70, 100, 100), width = 0, border_radius=20)
 
-
-class Person: 
-  def __init__(self, name, age, height, contact, address):
-    self.name= name
-    self.age= age
-    self.height= height
-    self.contact = contact
-    self.address = address
+  # Create a surface with transparency
+  #   transparent_surface = pygame.Surface((100, 100), pygame.SRCALPHA)
     
-  def to_dict(self):
-    ret = self.__dict__
-    ret['contact'] = self.contact.__dict__
-    ret["address"] = self.address.__dict__
-    return ret
+  # pygame.draw.rect(transparent_surface, (200, 255, 127, 100), (0, 0, 100, 100), width=0, border_radius=20)
 
-contact1 = Contact("7171-111-1111", "blah@gmail.com")
+  # # Blit the transparent surface onto the main screen
+  #   screen.blit(transparent_surface, (200, 70))
 
-address1 = Address("some st.", "Hershey", "PA", "17033")
+  # rect(surface, color, rect, width=0, border_radius=0, border_top_left_radius=-1, border_top_right_radius=-1, border_bottom_left_radius=-1, border_bottom_right_radius=-1) -> Rect
+    # RENDER YOUR GAME HERE
 
-dan = Person("Daniel", 12, "5'", contact1, address1)
+    # flip() the display to put your work on screen
+    pygame.display.flip()
 
-chris = Person("Christine", 16, "5'", contact1, address1)
+    clock.tick(60)  # limits FPS to 60
 
-# print(dan.__dict__)
-
-print(json.dumps(dan.address.__dict__, indent=4))
-
-print(json.dumps(chris.to_dict(), indent=4))
-# person = {
-#   "name": "adam",
-#   "age": 15,
-#   "height": "11 ft",
-#   "contacts": {
-#     "mobile": "717-222-1234",
-#     "work": "717-333-1234",
-#     "home": "717-444-1234",
-#     "weChat #": "from-hershey",
-#     "email": "sb@hms.com"
-#   },
-#   "address": {
-#     "street": "321 lollypop Dr",
-#     "city": "Dayton",
-#     "state": "OH",
-#     "country": "US",
-#     "zip": "17033"
-#   }
-# }
-
-# # get the mobil #
-
-# for phone, contact in person["contacts"].items():
-#   print(f"{phone}:{contact}")
-
-
-# person["name"] = "Bob"
-# print(person)
-
-# # person["nickname"] = "Bobby"
-# # print(person)
-# person2 = {
-#   "nickname": "Bobby",
-#   "ssn": "111-11-1111",
-#   "title": "Dr."
-# }
-
-
-# bob = person | person2
-# print(bob)
-
-
-# him = Person("joe", 44, "5'")
-# print(json.dumps(him.__dict__,  indent=5))
+pygame.quit()
